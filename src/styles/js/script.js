83fa32e0f24da81d2d352ef8e4c59e22234b52b4
Kabouss <kabouss.wahib@gmail.commkdir mon-projetcd mon-projetgit initecho>
2025-03-05 15:16:14 +0100

    maj 05/03

--- src/styles/js/script.js
+++ src/styles/js/script.js
@@ -1,26 +1,15 @@
-import Headroom from "headroom.js";
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.header__hamburger');
+  const menu = document.querySelector('#mainNav');
 
-const hamburger = document.querySelector(".menuBurger");
-const menu = document.querySelector(".menu");
-const page = document.body;
+  if (hamburger && menu) {
+    console.log('Elements found:', hamburger, menu);
 
-if (hamburger && menu) {
-  hamburger.addEventListener("click", () => {
-    const isOpen = hamburger.ariaExpanded === "true";
-    const isClosed = !isOpen;
-
-    hamburger.ariaExpanded = isClosed;
-    hamburger.classList.toggle("menuBurger--open", isClosed);
-    menu.ariaHidden = isOpen;
-    menu.classList.toggle("menu--open", isClosed);
-    page.classList.toggle("noscroll", isClosed);
-  });
-}
-
-const navBar = document.querySelector(".headroom");
-if (navBar) {
-  const headroom = new Headroom(navBar, {
-    offset: 200,
-  });
-  headroom.init();
-}
+    hamburger.addEventListener('click', () => {
+      console.log('Hamburger clicked');
+      menu.classList.toggle('active');
+    });
+  } else {
+    console.error('Elements not found');
+  }
+});
